8faceebc99eb706f64229f261bb5ff859d2b60ff
CodeErudition <infinity3nity360@gmail.com>
2024-03-14 19:26:13 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     all_name = dir(hidden_4)
     for name in all_name:
-        if all_name[:2] != "__":
+        if name[:2] != "__":
             print(name)
