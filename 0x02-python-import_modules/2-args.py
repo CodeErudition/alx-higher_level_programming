74ff8ef613e143bdb3b93703b6a92b58d115a346
CodeErudition <infinity3nity360@gmail.com>
2024-03-14 18:29:14 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} argument:".format(x))
+    elif x == 1:
+        print("{} argument:".format(x))
+    else:
+        print("{} arguments:".format(x))
+    for indx in range(1, x + 1):
+        print("{}: {:s}".format(indx, argv[indx])) 
