00dd00a1b1583daf20b7089dec3c2ae2b9a7c29c
CodeErudition <infinity3nity360@gmail.com>
2024-03-22 18:42:09 -0700

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,8 +3,8 @@
 def weight_average(my_list=[]):
     if not my_list:
         return (0)
-    num = 0;
-    div = 0;
+    num = 0
+    div = 0
 
     for elem in my_list:
         num += elem[0] * elem[1]
