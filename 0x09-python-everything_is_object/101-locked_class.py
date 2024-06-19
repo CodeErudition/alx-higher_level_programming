69892cc6a9a5662be6b7f3fd279a3e64064ef4c1
CodeErudition <infinity3nity360@gmail.com>
2024-06-19 08:09:08 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""This module defines a locked class"""
+
+
 class LockedClass():
+    """
+    This class represents a locked class restricts the creation
+    Attributes:
+        first_name (str): The first name of the instance,
+            initially set to None.
+   """
+
     __slots__ = ["first_name"]
+
     def __init__(self):
         self.first_name = None
