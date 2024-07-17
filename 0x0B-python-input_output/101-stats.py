5a97c915e9846175f5768bcb3f11a2e085dcf697
CodeErudition <infinity3nity360@gmail.com>
2024-07-17 08:54:40 -0700

    Debugging

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,7 +3,6 @@
 This script reads stdin line by line and computes metrics:
 """
 
-
 import sys
 
 
@@ -12,16 +11,16 @@ def print_stats(total_size, status_codes):
     Prints the current statistics: total file size and number of lines
     by status code
     """
-    print("File size:{}".format(total_size))
+    print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
+    print()
 
 
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
-
 
 try:
     for line in sys.stdin:
@@ -40,4 +39,5 @@ try:
 except KeyboardInterrupt:
     print_stats(total_size, status_codes)
     raise
+
 print_stats(total_size, status_codes)
