173382e68efb66f1c20707ddf97a33f114f72cf0
CodeErudition <infinity3nity360@gmail.com>
2024-07-20 16:52:42 -0700

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,10 +23,8 @@ class Rectangle(Base):
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-            x (int, optional): The x coordinate of the rectangle.
-                                Default is 0.
-            y (int, optional): The y coordinate of the rectangle.
-                                Default is 0.
+            x (int, optional): The x coordinate of the rectangle. Default is 0.
+            y (int, optional): The y coordinate of the rectangle. Default is 0.
             id (int, optional): The id of the instance. Default is None.
         """
         super().__init__(id)
@@ -35,12 +33,31 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    @staticmethod
+    def validator(attr, attr_value, greater_than_zero=True):
+        """
+        Validates the attribute value.
+
+        Args:
+            attr (str): The name of the attribute.
+            attr_value (int): The value of the attribute.
+            greater_than_zero (bool): If True, the value must be > 0.
+                                      If False, the value must be >= 0.
+        """
+        if not isinstance(attr_value, int):
+            raise TypeError("{} must be an integer".format(attr))
+        if greater_than_zero and attr_value <= 0:
+            raise ValueError("{} must be > 0".format(attr))
+        if not greater_than_zero and attr_value < 0:
+            raise ValueError("{} must be >= 0".format(attr))
+
     @property
     def width(self):
         return self.__width
 
     @width.setter
     def width(self, w_value):
+        self.validator("width", w_value)
         self.__width = w_value
 
     @property
@@ -49,6 +66,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, h_value):
+        self.validator("height", h_value)
         self.__height = h_value
 
     @property
@@ -57,6 +75,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x_value):
+        self.validator("x", x_value, False)
         self.__x = x_value
 
     @property
@@ -65,4 +84,5 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y_value):
+        self.validator("y", y_value, False)
         self.__y = y_value
