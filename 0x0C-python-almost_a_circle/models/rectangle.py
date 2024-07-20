20b648970c610f49203de35dc13c436f9c7f78c8
CodeErudition <infinity3nity360@gmail.com>
2024-07-20 16:11:14 -0700

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,34 +35,34 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, w_value):
-            self.__width = w_value
+    @width.setter
+    def width(self, w_value):
+        self.__width = w_value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, h_value):
-            self__height = h_value
+    @height.setter
+    def height(self, h_value):
+        self__height = h_value
 
-        @property
-        def x(self):
-            return self.__x
+    @property
+    def x(self):
+        return self.__x
 
-        @x.setter
-        def x(self, x_value):
+    @x.setter
+    def x(self, x_value):
             self.__x = x_value
 
-        @property
-        def y(self):
-            return self.__y
+    @property
+    def y(self):
+        return self.__y
 
-        @y.setter
-        def y(self, y_value):
-            self.__y = y_value
+    @y.setter
+    def y(self, y_value):
+    self.__y = y_value
